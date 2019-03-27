0b7a25fc973562db56a0df54f57106e238104b77
abichat <a.bichat@yahoo.fr>
2019-03-27 09:09:25 +0100

    :memo:

--- R/derived_measures.R
+++ R/derived_measures.R
@@ -2,6 +2,18 @@
 #'
 #' Measures derived from confusion matrix.
 #'
+#' TPR: True Positive Rate, also known as Sensitivity and Recall.
+#'
+#' TNR: True Negative Rate, also known as Specificity and Selectivity.
+#'
+#' PPV: Positive Predictive Value, also known as Precision.
+#'
+#' FDR: False Discovery Rate.
+#'
+#' ACC: Accuracy.
+#'
+#' F1: F1 score.
+#'
 #' @param detected Vector of elements that are detected.
 #' @param true Vector of element that are supposed to be detected (\eqn{H1}).
 #' @param all Vector of all elements.
@@ -25,6 +37,12 @@ ebc_TNR <- function(detected, true, all, m = length(all)){
 
 #' @rdname ebc_TPR
 #' @export
+ebc_PPV <- function(detected, true){
+  ebc_TP(detected, true) / (ebc_TP(detected, true) + ebc_FP(detected, true))
+}
+
+#' @rdname ebc_TPR
+#' @export
 ebc_FDR <- function(detected, true){
   ebc_FP(detected, true) / max(length(detected), 1)
 }
@@ -33,12 +51,6 @@ ebc_FDR <- function(detected, true){
 #' @export
 ebc_ACC <- function(detected, true, all, m = length(all)){
   (ebc_TP(detected, true) + ebc_TN(detected, true, m = m)) / m
-}
-
-#' @rdname ebc_TPR
-#' @export
-ebc_PPV <- function(detected, true){
-  ebc_TP(detected, true) / (ebc_TP(detected, true) + ebc_FP(detected, true))
 }
 
 #' @rdname ebc_TPR
